5af00a765deeee23ef9a6687d783de19b6ed1329
Hooward0012 <944787245@qq.com>
2024-10-28 14:51:59 +0800

    原始字符

--- 字符串.py
+++ 字符串.py
@@ -2,7 +2,15 @@
 String 是不可变类型的，它是一种类似于列表的数据结构，
 比如 “handsomeb” 这个字符串，你可以通过索引来访问字符串里面的字符
 '''
-str = 'handsomeb'
-print(str[0])
-print(str[1])
-print(str[2])
+# str = 'handsomeb'
+# print(str[0])
+# print(str[1])
+# print(str[2])
+
+'''
+使用r来显示原始字符
+'''
+str = '1234\n567'
+print(str)
+str1 = r'1234\n567'
+print(f'str1:{str1}')
