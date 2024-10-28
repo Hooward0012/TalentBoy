61ad6a3df1c0044d9693e9e3c9adecfa29ee82ac
Hooward0012 <944787245@qq.com>
2024-10-28 15:02:27 +0800

    字符串格式化

--- 字符串.py
+++ 字符串.py
@@ -18,11 +18,28 @@ String 是不可变类型的，它是一种类似于列表的数据结构，
 '''
 字符串拼接
 '''
-str1 = 'fxxk'
-str2 = 'python'
+# str1 = 'fxxk'
+# str2 = 'python'
+#
+# str1 += str2
+# str1 = str1 + str2
+#
+# text = 'fxxk' 'python' '!!!'
+# print(text)
 
-str1 += str2
-str1 = str1 + str2
+'''
+格式化
+1、 %s ： 字符格式化
 
-text = 'fxxk' 'python' '!!!'
-print(text)
+2、 %d : 整型格式化
+
+3、 format方法
+
+4、 f-string
+'''
+print('大家好，我叫%s,今年 %d 岁！'%('howard', 18))
+print('大家好，我叫{0}, 我今年{1}岁 ！'.format('howard', 18))
+print('大家好，我叫{name}, 我今年{age}!'.format(name='howard', age = 18))
+name = 'howard'
+age = 18
+print(f'大家伙，我叫{name}, 今年{age}岁')
