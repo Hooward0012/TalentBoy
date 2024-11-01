2dfdd053bedb90cd8311325b41fe898c9d0b00b7
Howard0012 <944787245@qq.com>
2024-11-01 20:09:42 +0800

    lambda的用法

---
+++ lambda.py
@@ -0,0 +1,12 @@
+my_list = [1,2,3,4,5]
+
+# def add(x):
+#     return x+5
+
+# print(list(map(add,my_list)))
+#
+# print(list(map(lambda x: x+ 5, my_list)))
+#
+# print(list(map(lambda x: x % 2==0, my_list)))
+
+(lambda x: print(x + 2))(2)
